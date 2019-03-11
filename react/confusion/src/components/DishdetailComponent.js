d93e12da3d42553a553e8944022eff837f5fb7ea
jiclotus <joseignaciocastelli92@gmail.com>
2019-03-10 21:49:33 -0300

    <div className=row>

--- react/confusion/src/components/DishdetailComponent.js
+++ react/confusion/src/components/DishdetailComponent.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -60,14 +62,26 @@ const  DishDetail = (props) => {
 
     if (props.dish != null)
         return (
+            <div className="container">
             <div className="row">
-                {RenderDish(props.dish)}
-                <div className="col-12 col-md-5 m-2">
-                    <h4>Comments</h4>
-                    <div className="ul list-unstyled">
-                        {RenderComments(props.dish.comments)}
-                    </div>
+                <Breadcrumb>
+
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>                
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
                 </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
             </div>
         );
     else
